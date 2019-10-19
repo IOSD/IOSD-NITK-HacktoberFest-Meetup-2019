125091592d8eddd87c92227707d7e4beef03bbae
usernameharshchauhan <56071758+usernameharshchauhan@users.noreply.github.com>
2019-10-19 23:11:09 +0530

    odd_evenPR.cpp

---
+++ odd_evenPR.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+main()
+{
+	int z;
+	printf("This is a program to find whether given number is odd or even\nEnter the number = ");
+	scanf("%d",&z);
+	if(z%2==0)
+	printf("given number is EVEN");
+	else
+	printf("given number is ODD");
+}
