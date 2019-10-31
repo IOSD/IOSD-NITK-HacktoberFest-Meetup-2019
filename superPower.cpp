f81bb1f3bbae548b62bfe17948990823d33f24e9
Rahul Garg <52508738+rahul069@users.noreply.github.com>
2019-10-31 21:16:12 +0530

    my fifth PR

---
+++ superPower.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    float base, exponent, ;
+
+
+    cin >> base >> exponent;
+
+    //result = pow(base, exponent);
+    for(int i = 0 ;i<=exponent;i++)
+    {
+        result =
+    }
+
+    cout << result;
+
+    return 0;
+}
